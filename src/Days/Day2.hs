551509350480ffe23493c793b0a6b48d2f22b529
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-12-02 07:55:02 +0100

    Day 2

--- src/Days/Day2.hs
+++ src/Days/Day2.hs
@@ -1,18 +1,35 @@
 module Days.Day2 ( day2 ) where
 
-import AOC.Day ( Day(..) )
+import AOC.Day   ( Day(..) )
+import Data.Char ( toUpper )
 
-type Input = [Int]
-type Output = String
+data Command = Up Int | Down Int | Forward Int deriving (Show, Read)
+type Coordinates = (Int, Int, Int)
+type Input = [Command]
+type Output = Int
 
 parse :: String -> Input
-parse = undefined
+parse = map (read . capitalized) . lines
+    where capitalized s = toUpper (head s) : tail s
+
+update :: Coordinates -> Command -> Coordinates
+update (x, y, aim) (Up n)      = (x, (y - n), aim)
+update (x, y, aim) (Down n)    = (x, (y + n), aim)
+update (x, y, aim) (Forward n) = ((x + n), y, aim)
+
+updateAim :: Coordinates -> Command -> Coordinates
+updateAim (x, y, aim) (Up n)      = (x, y, (aim - n))
+updateAim (x, y, aim) (Down n)    = (x, y, (aim + n))
+updateAim (x, y, aim) (Forward n) = ((x + n), (y + aim*n), aim)
+
+getResult :: Coordinates -> Output
+getResult (x, y, _) = x * y
 
 partA :: Input -> Output
-partA = const "TODO"
+partA = getResult . foldl update (0, 0, 0)
 
 partB :: Input -> Output
-partB = const "TODO"
+partB = getResult . foldl updateAim (0, 0, 0)
 
 day2 :: Day
 day2 = Day 2 parse partA partB
