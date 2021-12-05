33db1cf71bb24ccbd7eccf01cc9ea77cd907edd4
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-12-05 14:58:55 +0100

    Day 5

--- src/Days/Day5.hs
+++ src/Days/Day5.hs
@@ -1,18 +1,43 @@
 module Days.Day5 ( day5 ) where
 
-import AOC.Day ( Day(..) )
+import AOC.Day         ( Day(..) )
+import Data.List       ( (\\), group, sort, sortOn )
+import Data.List.Split ( splitOn )
 
-type Input = [Int]
-type Output = String
+type Point   = (Int, Int)
+type Segment = (Point, Point)
+type Input   = [Segment]
+type Output  = Int
 
 parse :: String -> Input
-parse = undefined
+parse = map parseSegment . lines
+    where parseSegment   = toTuple . map parsePoint . splitOn " -> "
+          parsePoint   p = read $ concat ["(", p, ")"]
+          toTuple [x, y] = (x, y)
+
+toPoints :: Segment -> [Point]
+toPoints ((x1, y1), (x2, y2))
+    | x1 == x2  = [(x1, y) | y <- y1...y2]
+    | y1 == y2  = [(x, y1) | x <- x1...x2]
+    | otherwise = zip (x1...x2) (y1...y2)
+
+(...) :: Int -> Int -> [Int]
+x1 ... x2
+    | x1 <= x2  = [x1..x2]
+    | otherwise = reverse [x2..x1]
+
+histogram :: Ord a => [a] -> [(a, Int)]
+histogram = map (\g -> (head g, length g)) . group . sort
+
+countDangerZones :: Input -> Int
+countDangerZones = length . filter ((> 1) . snd) . histogram . concatMap toPoints
 
 partA :: Input -> Output
-partA = const "TODO"
+partA = countDangerZones . filter isPerpendicular
+    where isPerpendicular ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
 
 partB :: Input -> Output
-partB = const "TODO"
+partB = countDangerZones
 
 day5 :: Day
 day5 = Day 5 parse partA partB
