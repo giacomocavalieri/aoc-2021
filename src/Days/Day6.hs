b520e311c4701929b8a300551dd6dae628d22242
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-12-06 07:30:36 +0100

    Day 6

--- src/Days/Day6.hs
+++ src/Days/Day6.hs
@@ -1,18 +1,34 @@
 module Days.Day6 ( day6 ) where
 
-import AOC.Day ( Day(..) )
+import AOC.Day   ( Day(..) )
+import Data.List ( group, sort )
+import Data.Map  ( Map )
+import qualified Data.Map as M ( elems, findWithDefault, fromList, insertWith, mapKeysWith )
 
-type Input = [Int]
-type Output = String
+type Input = Map Int Int
+type Output = Int
 
 parse :: String -> Input
-parse = undefined
+parse s = histogram $ read $ concat ["[", s, "]"]
+    where histogram = M.fromList . map (\g -> (head g, length g)) . group . sort 
+ 
+update :: Input -> Input
+update xs = M.insertWith (+) 8 zeros decreased 
+    where zeros      = M.findWithDefault 0 0 xs
+          decrease k = if k > 0 then k - 1 else 6
+          decreased  = M.mapKeysWith (+) decrease xs
+
+totalPopulation :: Input -> Int
+totalPopulation = sum . M.elems
+
+nthGeneration :: Int -> Input -> Input
+nthGeneration n = (!!n) . iterate update 
 
 partA :: Input -> Output
-partA = const "TODO"
+partA = totalPopulation . nthGeneration 80
 
 partB :: Input -> Output
-partB = const "TODO"
+partB = totalPopulation . nthGeneration 256
 
 day6 :: Day
 day6 = Day 6 parse partA partB
