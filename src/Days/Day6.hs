ca45fdabe92a1354a17969bf16441e61640b69b7
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-12-06 10:05:05 +0100

    Day 6

--- src/Days/Day6.hs
+++ src/Days/Day6.hs
@@ -1,34 +1,28 @@
 module Days.Day6 ( day6 ) where
 
-import AOC.Day   ( Day(..) )
-import Data.List ( group, sort )
-import Data.Map  ( Map )
-import qualified Data.Map as M ( elems, findWithDefault, fromList, insertWith, mapKeysWith )
+import AOC.Day         ( Day(..) )
+import Data.List.Split ( splitOn )
+import Data.MultiSet   ( MultiSet )
+import qualified Data.MultiSet as M ( concatMap, fromList, size )
 
-type Input = Map Int Int
-type Output = Int
+type TimeToSpawn = Int
+type Input       = MultiSet TimeToSpawn
+type Output      = Int
 
 parse :: String -> Input
-parse s = histogram $ read $ concat ["[", s, "]"]
-    where histogram = M.fromList . map (\g -> (head g, length g)) . group . sort 
+parse = M.fromList . map read . splitOn ","
  
 update :: Input -> Input
-update xs = M.insertWith (+) 8 zeros decreased 
-    where zeros      = M.findWithDefault 0 0 xs
-          decrease k = if k > 0 then k - 1 else 6
-          decreased  = M.mapKeysWith (+) decrease xs
+update = M.concatMap (\i -> if i == 0 then [6, 8] else [i-1])
 
-totalPopulation :: Input -> Int
-totalPopulation = sum . M.elems
-
-nthGeneration :: Int -> Input -> Input
-nthGeneration n = (!!n) . iterate update 
+nthGeneration :: Int -> Input -> Output
+nthGeneration n = M.size . (!!n) . iterate update 
 
 partA :: Input -> Output
-partA = totalPopulation . nthGeneration 80
+partA = nthGeneration 80
 
 partB :: Input -> Output
-partB = totalPopulation . nthGeneration 256
+partB = nthGeneration 256
 
 day6 :: Day
 day6 = Day 6 parse partA partB
